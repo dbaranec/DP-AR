3bee27a2e954333e8e8085b5cc27c00aa411bf67
Dominik Baranec <dbaranec@MacBook-Pro.local>
2016-12-02 18:31:31 +0100

    spider add

---
+++ Assets/vbScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler   {
+
+	private GameObject vbButtonObject;
+	// Use this for initialization
+	void Start () {
+
+		vbButtonObject = GameObject.Find ("actionButton");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+	}
+	
+
+	public void OnButtonPressed  (VirtualButtonAbstractBehaviour vb) {
+		Debug.Log ("Button Down");	
+	}
+
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+		Debug.Log ("Button up");
+	}
+}
