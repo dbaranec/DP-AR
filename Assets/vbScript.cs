20780ff121bf158335f7e41a7a7e226b8813563a
Dominik Baranec <dbaranec@MacBook-Pro.local>
2016-12-02 21:46:13 +0100

    add button
    hide/show spider activeted

--- Assets/vbScript.cs
+++ Assets/vbScript.cs
@@ -5,21 +5,37 @@ using Vuforia;
 public class vbScript : MonoBehaviour, IVirtualButtonEventHandler   {
 
 	private GameObject vbButtonObject;
+	private GameObject spider;
 	// Use this for initialization
 	void Start () {
-
+		spider = GameObject.Find ("spider");
 		vbButtonObject = GameObject.Find ("actionButton");
 		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		ReInvoke ();//vyvolavanie attaku
 
 	}
-	
+
+	private void Repeat(){
+		spider.GetComponent<Animation> ().Play();
+	}
+	private void ReInvoke(){
+		InvokeRepeating("Repeat", 1.0f, 1.0f);
+	}
+
 
 	public void OnButtonPressed  (VirtualButtonAbstractBehaviour vb) {
-		Debug.Log ("Button Down");	
+			
+		spider.active = true;
+		//ked sa stlaci bude visible
+	//	spider.SetActive(true);
+			
+
 	}
 
 
 	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
-		Debug.Log ("Button up");
+		spider.active=false;
+		//ked pustim bude invisible
+
 	}
 }
